e7584b4a49e90702166e31ed668d5682dca24262
Jill  Rayca <rkrayca@yahoo.com>
2018-07-25 23:33:33 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -1,3 +1,7 @@
 puts "Where would you like to stay?"
 place_stay=gets.chomp
 puts "You would like to stay at #{place_stay}"
+
+puts "what is your username?"
+name=gets.chomp
+puts "Your username is #{name}"
